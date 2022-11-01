f9f8e67cdd228d8f2d5f6a397a7be9b64e267b1b
natana <natana@localhost.localdomain>
2022-11-01 10:35:34 +0300

    Refined Feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,29 @@
 import "./Feed.css"
-function Feed(){
+import placeholdr from './assets/img/natana12.jpeg'
+import mascot from './assets/img/mascotuhd.png'
+
+function Feed() {
 
     return (
-        <div className="Feed">Done</div>
+        <div className="Feed">
+            <div className="Headnote">
+                <img src={mascot} />
+                <span>Natana Museum</span>
+            </div>
+            <div className="Exhibition">
+            <FeedImg image={placeholdr}/>
+            </div>
+        </div>
+    )
+}
+
+function FeedImg(){
+    const image=placeholdr
+    return(
+        <div>
+            
+        <img src={image} /> 
+        </div>  
     )
 }
 
